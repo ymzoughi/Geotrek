610de9dba531c1b4c5fdfb2bb1d03f55bb30c0ad
ymz <yahya.mzoughi@makina-corpus.com>
2013-09-30 10:24:57 +0200

    init procject

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+$(".nav a").click(function(){
+	sliceTo($(this));
+});
+$(window).scroll(function() {
+	//var h = document.getElementsByTagName("H1");
+	//for (var i = 0; i < h.length; i++) {
+		//if (h[i].getBoundingClientRect().top<127 && h[i].getBoundingClientRect().top>25 ) {
+			//console.log(h[i].id);
+		//	sliceTo($('[data-spy ='+h[i].id+']'));
+		//}
+//	}
+	
+//console.log(document.getElementById('h1'));
+//console.log (h.getBoundingClientRect());
+});
+function sliceTo(elm)
+{
+	var To = $(elm).offset().left;
+	var width = $(elm).width();
+	$('.top-bar').animate({
+    	'left': To+15,
+    	'width':width,
+  		},"slow", function() {
+  			$(elm).css('color','##bdce12');
+  	});
+}
